096948a16cd356b06dfa9ae2d4b5e03dadcf1aa6
bmodena <bmodena@sussind.com>
2016-04-18 15:54:53 -0600

    gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,11 @@
     var inlineCss = require('gulp-inline-css');
     var autoprefixer = require('gulp-autoprefixer');
     var plumber = require('gulp-plumber');
-    var rename = require('gulp-rename');
     var notify = require('gulp-notify');
     var beep = require('beepbeep');
     var watch = require('gulp-watch');
   } catch( e ) {
-    console.log('Could not find one of the packages gulp needs to run.  Please run `npm install` to see if that resolves the issue.  The error is below:');
+    console.log('Could not find one of the packages gulp needs to run.');
     console.log(e);
     return false;
   }
@@ -25,14 +24,18 @@
       this.emit('end');
   };
 
+  // SCSS config
   var input = 'styles/main.scss';
   var output = '.build/css';
   var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
   };
+
+  // HTML email templates config
   var templates = './*.html';
 
+  // Compile scss files
   gulp.task('styles', function () {
     return gulp
       .src(input)
@@ -48,19 +51,14 @@
           .pipe(gulp.dest('build/'));
   });
 
+  // Watch scss files and .html templates
   gulp.task('watch', function() {
-    return gulp
-      // Watch the scss file for a change,
-      .watch(input, ['styles'])
-      .on('change', function(event) {
+      gulp.watch(input, ['styles']).on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
       });
-      /*
-      .watch(templates, ['inline'])
-      .on('change', function(event) {
+      gulp.watch(templates, ['inline']).on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
       });
-      */
 
   });
 
